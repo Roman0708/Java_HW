d9409c158622e3ff1712681a6d838cb08546d38c
Roman Kalimov <Roman.Kalimov@icl-services.com>
2023-02-03 19:05:00 +0300

    p2 and 3 done

--- Java_HW/Java_HW_4.java
+++ Java_HW/Java_HW_4.java
@@ -1,17 +1,16 @@
-import java.io.BufferedReader;
+
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+
 
 public class Java_HW_4 {
     public static void main(String[] args) {
         FileWriter writer = null;
-        FileReader reader = null;
+        // FileReader reader = null;
         String answer = "";
 
         // 1
@@ -35,49 +34,11 @@ public class Java_HW_4 {
         // 2
 
         try {
-
-            
-            
-            
-            // create a buffer for all rows from file
-            // ArrayList<String> buffer = new ArrayList<>();
-            // BufferedReader br = new BufferedReader(new FileReader("List.txt"));
-            
-            // ArrayList<String> list = new ArrayList<>();
-            // int counter = 0;
-            // String line = br.readLine();
-            // while (line != null) {
-
-
-            //     String[] parts = line.split(" "); //
-            //     list.add(parts.toString()); //
-            //     //Fill the buffer
-
-            //     buffer.add(line);
-            //     line = br.readLine();
-            //     counter++;
-            // }
-            
-            // System.out.println(list);
-            
-            // for (String s : buffer) {
-
-            //     //Loop the buffer and parse each row data, split by space
-
-            //     String[] parts = s.split(" ");
-
-            //     System.out.println(Arrays.toString(parts));
-
-            // }
-
-            // System.out.println(buffer);
-
-
             System.out.println();
             FileReader fr = new FileReader("List.txt");
             String text = "";
             while (fr.ready()) {
-                text += (char)fr.read();
+                text += (char) fr.read();
             }
             System.out.println(text);
 
@@ -85,33 +46,34 @@ public class Java_HW_4 {
             String[] parts = text.split("\r\n");
             System.out.println(Arrays.toString(parts));
 
-            
+            ArrayList<String> name = new ArrayList<>();
+            ArrayList<String> surname = new ArrayList<>();
+            ArrayList<String> lastname = new ArrayList<>();
+            ArrayList<Integer> age = new ArrayList<>();
+            ArrayList<Boolean> gender = new ArrayList<>();
+            ArrayList<Integer> index = new ArrayList<>();
 
 
-
-
-            ArrayList<String> list = new ArrayList<>();
-
-            for (String temp: parts) {
-                list.add(temp);
+            for (String s: parts) {
+                String[] help = s.split(" ");
+                surname.add(help[0]);
+                name.add(help[1]);
+                lastname.add(help[2]);
+                age.add(Integer.parseInt(help[3]));
+                gender.add(help[4].contains("F")?false:true);
+                System.out.println(Arrays.toString(help));
             }
 
-            System.out.println(list.toString());
 
-            for (int i = 0; i < list.size(); i++) {
-                String str = parts[i].split(" ").toString();
-                list.set(i, str); 
+
+            for (int i = 0; i < parts.length; i++) {
+                System.out.println(surname.get(i) + " " + name.get(i).charAt(0)+ ". " + lastname.get(i).charAt(0) + ". " + age.get(i) + " " + (gender.get(i)?("M"):"F"));
             }
 
-            System.out.println(list.toString());
-
-            // // System.out.print(Arrays.toString(parts));
-            // br.close();
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
     }
-
 }
